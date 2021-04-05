f6ad7ec9be52de6e4c8d5da7dbf32dace163cc08
JS <jeff.shep777@gmail.com>
2021-04-05 11:24:40 -0600

    Form udpates

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     super();
 
     this.state = {
-      reminders: '',
+      reminders: [],
     };
   }
 
@@ -17,12 +17,18 @@ class App extends Component {
     this.setState({reminders: testApiData.reminders});
   }
 
+  addReminder(reminder) {
+    // console.log([this.state.reminders, reminder])
+    testApiData.reminders.push(reminder)
+    this.setState({ reminders: testApiData.reminders });
+  }
+
   render() {
     return (
       <main>
         <h1>To-Dos</h1>
-        {this.state.reminders && <ToDos reminders={this.state.reminders} />}
-        <Form />
+        {this.state.reminders.length && <ToDos reminders={this.state.reminders} />}
+        <Form addReminder= {this.addReminder}/>
       </main>
     );
   }
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -18,12 +18,19 @@ class Form extends Component {
     this.setState({note})
   }
 
+  submitReminder(event) {
+    event.preventDefault(event)
+    const reminder = {date: this.state.date, note: this.state.note, id: Date.now()}
+    this.props.addReminder(reminder)
+  }
+
+
   render() {
     return (
       <form>
         <input placeholder="Date" onChange={(event) => this.handleDateChange(event.target.value)}></input>
         <input placeholder="Reminder" onChange={(event) => this.handleNoteChange(event.target.value)}></input>
-        <button>Add</button>
+        <button onClick={(event) => this.submitReminder(event)} >Add</button>
       </form>
 
     )
